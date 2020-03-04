80f67c3466d09e928ff8ddfe1fbda476d2cb0bb0
erictanggit <eric.tang@dauphine.eu>
2020-03-04 22:40:55 +0100

    reorganize import and package

--- src/test/java/io/github/erictanggit/main/ActionPerformTest.java
+++ src/test/java/io/github/erictanggit/main/ActionPerformTest.java
@@ -1,6 +1,11 @@
 package io.github.erictanggit.main;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
--- src/test/java/io/github/erictanggit/rules/RulesTest.java
+++ src/test/java/io/github/erictanggit/rules/RulesTest.java
@@ -1,6 +1,8 @@
 package io.github.erictanggit.rules;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class RulesTest {
